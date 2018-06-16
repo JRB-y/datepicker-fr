0f3c81848c59dda8fce47753f69ce5279e8befda
Jerbi Youssef <jrb@iMac-de-Jerbi.local>
2018-06-16 15:18:53 +0100

    datepicker FR

---
+++ datepicker-fr.js
@@ -0,0 +1,28 @@
+/**
+ * Bootstrap datepicker fr-FR
+ * 
+ * todayHighlight
+ * datesDisabled
+ * startDate
+ */
+
+$.fn.datepicker.dates['fr'] = {
+    days: ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
+    daysShort: ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
+    daysMin: ["d", "l", "ma", "me", "j", "v", "s"],
+    months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
+    monthsShort: ["janv.", "févr.", "mars", "avril", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."],
+    today: "Aujourd'hui",
+    monthsTitle: "Mois",
+    clear: "Effacer",
+    weekStart: 1,
+    format: "dd/mm/yyyy"
+};
+
+$('#date-picker').datepicker({
+    language: 'fr',
+    todayHighlight: true,
+    datesDisabled: '02/06/2018',
+    startDate: '+0d',
+});
+
